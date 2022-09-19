b08ac755c39ac7d46ce840eed375f794d0e5ca53
n.trifonov <nikita.trifonov@flocktory.com>
2022-09-19 17:38:48 +0300

    [les-2.1] AppRouter. Конфиг для роутера

---
+++ src/app/providers/router/ui/routeConfig.tsx
@@ -0,0 +1,18 @@
+import {RouteProps} from "react-router-dom";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
+import {AppRoutes, RoutePath} from "shared/lib/routePath/routePath";
+
+
+
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+    [AppRoutes.MAIN]: {
+        path: RoutePath.main,
+        element: <MainPage/>
+    },
+    [AppRoutes.ABOUT]: {
+        path: RoutePath.about,
+        element: <AboutPage/>
+    },
+
+}
