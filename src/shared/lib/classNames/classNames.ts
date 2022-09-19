c37428391b15591e79a9e8d6741ac9e3249b83cc
n.trifonov <nikita.trifonov@flocktory.com>
2022-09-19 18:48:36 +0300

    [les-2.2] Navbar

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,11 @@
 type Mods = Record<string, boolean | string>
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods)
-            .filter(([cls, value]) => Boolean(value)) //[cls, value] - потому, что entries возвращает массив кортежей
-            .map(([cls, value]) => cls)
+                 .filter(([cls, value]) => Boolean(value)) //[cls, value] - потому, что entries возвращает массив кортежей
+                 .map(([cls, value]) => cls)
     ].join(' ')
 }
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,39 @@
+import React, {FC} from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss'
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string,
+}
+
+export const Navbar: FC<NavbarProps> = (props: NavbarProps) => {
+    const {className} = props
+
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink
+                    to={`/`}
+                    className={cls.mainLink}
+                    theme={AppLinkTheme.PRIMARY}>
+                    Главная
+                </AppLink>
+                <AppLink
+                    to={`/about`}
+                    theme={AppLinkTheme.PRIMARY}
+                >О сайте
+                </AppLink>
+            </div>
+
+        </div>
+    );
+};
+
+
+
+
+
+
+
+
