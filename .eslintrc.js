b276c2bd43a694c3ca0ec7e8c16b6880b79824fd
n.trifonov <nikita.trifonov@flocktory.com>
2022-09-27 17:59:43 +0300

    [les-2.9] eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next',
+    ],
+    rules: {
+        'max-len': [2, 120, { ignoreComments: true }],
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        indent: [2, 4],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'react/function-component-definition': 'off',
+        'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
+        'no-use-before-define': [2, { functions: false, classes: false }],
+        'space-before-function-paren': [2, 'never'],
+        'brace-style': [2, '1tbs', { allowSingleLine: false }],
+        'comma-style': [2, 'last'],
+        'comma-dangle': [2, 'always-multiline'],
+        'i18next/no-literal-string': [2, { markupOnly: true }],
+    },
+    globals: {
+        __IS_DEV__: true,
+    },
+};
