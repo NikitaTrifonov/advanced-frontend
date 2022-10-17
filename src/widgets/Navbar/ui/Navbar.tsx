6d7e711f470e0239e62f3423f3ad9c7b9414a1e8
n.trifonov <nikita.trifonov@flocktory.com>
2022-10-17 17:48:58 +0300

    upgrade sidebar, add button themes

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -11,26 +9,9 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = (props: NavbarProps) => {
     const { className } = props;
 
-    const { t } = useTranslation();
-
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink
-                    to="/"
-                    className={cls.mainLink}
-                    theme={AppLinkTheme.SECONDARY}
-                >
-                    {t('Главная')}
-                </AppLink>
-                <AppLink
-                    to="/about"
-                    theme={AppLinkTheme.SECONDARY}
-                >
-                    {t('О сайте')}
-                </AppLink>
-            </div>
-
+            <div className={cls.links} />
         </div>
     );
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,8 +2,12 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import React, { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/lib/routePath/routePath';
+import AboutIcon from 'shared/assets/icons/about20-20.svg';
+import MainIcon from 'shared/assets/icons/main20-20.svg';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -24,12 +28,37 @@ export const Sidebar = ({ className }: SidebarProps) => {
             <Button
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
+                className={cls.collapseBtn}
+                theme={ButtonTheme.BACKGROUND_INVERTED}
+                square
+                size={ButtonSize.L}
             >
-                {t('Переключить')}
+                {collapsed ? '>' : '<'}
             </Button>
+            <div className={cls.items}>
+                <AppLink
+                    to={RoutePath.main}
+                    className={cls.item}
+                    theme={AppLinkTheme.SECONDARY}
+                >
+                    <MainIcon className={cls.icon} />
+                    <span className={cls.link}>{t('Главная')}</span>
+                </AppLink>
+                <AppLink
+                    to={RoutePath.about}
+                    className={cls.item}
+                    theme={AppLinkTheme.SECONDARY}
+                >
+                    <AboutIcon className={cls.icon} />
+                    <span className={cls.link}>{t('О сайте')}</span>
+                </AppLink>
+            </div>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
+                <LangSwitcher
+                    className={cls.lang}
+                    short={collapsed}
+                />
             </div>
         </div>
     );
