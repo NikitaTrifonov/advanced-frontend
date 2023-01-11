fb687aa7adaef9658a2de6140de6dcfc9b582708
n.trifonov <nikita.trifonov@flocktory.com>
2023-01-11 17:35:34 +0300

    add skeleton, code, articledetails card

---
+++ src/entities/Article/model/selectors/articleDetails.test.ts
@@ -0,0 +1,50 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getArticleDetailsData, getArticleDetailsIsError, getArticleDetailsIsLoading } from './articleDetails';
+
+describe('getArticleDetails.test', () => {
+    test(' should return articleDetails', () => {
+        const data = {
+            id: '1',
+            title: 'subtitle',
+        };
+        const state: DeepPartial<StateSchema> = {
+            articleDetails: {
+                data,
+            },
+        };
+        expect(getArticleDetailsData(state as StateSchema)).toEqual(data);
+    });
+
+    test(' should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getArticleDetailsData(state as StateSchema)).toEqual(undefined);
+    });
+
+    test(' should return isLoading', () => {
+        const state: DeepPartial<StateSchema> = {
+            articleDetails: {
+                isLoading: true,
+            },
+        };
+        expect(getArticleDetailsIsLoading(state as StateSchema)).toEqual(true);
+    });
+
+    test(' should work with empty state isLoading', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getArticleDetailsIsLoading(state as StateSchema)).toEqual(false);
+    });
+
+    test(' should return error', () => {
+        const state: DeepPartial<StateSchema> = {
+            articleDetails: {
+                error: 'error',
+            },
+        };
+        expect(getArticleDetailsIsError(state as StateSchema)).toEqual('error');
+    });
+
+    test(' should work with empty state error', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getArticleDetailsIsError(state as StateSchema)).toEqual(undefined);
+    });
+});
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { ArticleDetails } from 'entities/Article';
+import { useParams } from 'react-router-dom';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
@@ -8,12 +10,21 @@ interface ArticleDetailsPageProps {
 }
 
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
-    const { t } = useTranslation('articles');
+    const { t } = useTranslation('article-details');
     const { className } = props;
+    const { id } = useParams<{ id: string }>();
+
+    if (!id) {
+        return (
+            <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                {t('Статья не найдена')}
+            </div>
+        );
+    }
 
     return (
         <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-            ArticleDetailsPage
+            <ArticleDetails id={id} />
         </div>
     );
 };
