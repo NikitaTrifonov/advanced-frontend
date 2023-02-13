1807e4e0b4f32c5ddc251a75793b98d51974f6be
n.trifonov <nikita.trifonov@flocktory.com>
2023-02-13 16:09:51 +0300

    production deploy

--- src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
+++ src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
@@ -1,8 +1,4 @@
 import { FC, lazy } from 'react';
 import { AddCommentFormProps } from './AddCommentForm';
 
-export const AddCommentFormAsync = lazy<FC<AddCommentFormProps>>(() => new Promise((resolve) => {
-    // @ts-ignore
-    // Искусственная задержка, что бы увидеть fallback(анимация загрузки) в <Suspense>
-    setTimeout(() => resolve(import('./AddCommentForm')), 1500);
-}));
+export const AddCommentFormAsync = lazy<FC<AddCommentFormProps>>(() => import('./AddCommentForm'));
--- src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.async.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-export const ArticleEditPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // Искусственная задержка, что бы увидеть fallback(анимация загрузки) в <Suspense>
-    setTimeout(() => resolve(import('./ArticleEditPage')), 1500);
-}));
+export const ArticleEditPageAsync = lazy(() => import('./ArticleEditPage'));
