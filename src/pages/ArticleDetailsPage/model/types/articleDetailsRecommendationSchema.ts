e373aaeb39cdd7ea145916a3a2b65b58889b976e
n.trifonov <nikita.trifonov@flocktory.com>
2023-02-13 16:19:05 +0300

    fix production deploy

--- src/pages/ArticleDetailsPage/model/types/articleDetailsRecommendationSchema.ts
+++ src/pages/ArticleDetailsPage/model/types/articleDetailsRecommendationSchema.ts
@@ -1,5 +1,5 @@
-import { EntityState } from '@reduxjs/toolkit';
 import { Article } from 'entities/Article';
+import { EntityState } from '@reduxjs/toolkit';
 
 export interface ArticleDetailsRecommendationSchema extends EntityState<Article> {
     isLoading?: boolean;
